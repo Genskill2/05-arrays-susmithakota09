54560720c972fcfa06821bc4dd7f75e89e5a294a
susmithakota09 <85020518+susmithakota09@users.noreply.github.com>
2021-06-19 12:16:06 +0530

    test_min.c

--- test_min.c
+++ test_min.c
@@ -2,13 +2,16 @@
 #include <assert.h>
 
 
-int min(int [], int);
 
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert (min(x, 7) == -3);
-
-  int y[] = {5};
-  assert (min(y, 1) == 5);
-  printf("Min: passed\n");
+int min(int arr[],int count)
+{
+  int minn=arr[0];
+  for(int i=1;i<count;i++)
+  {
+    if(arr[i] < minn)
+    {
+      minn = arr[i];
+    }
   }
+  return minn;
+}
