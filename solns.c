70e6cf07e3e33e6744dc42d18a2519324d9975e0
susmithakota09 <85020518+susmithakota09@users.noreply.github.com>
2021-06-19 12:10:52 +0530

    solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,12 @@
-/* Enter your solutions in this file */
-#include <stdio.h>
+int max(int arr[],int count)
+{
+  int maxx=0;
+  for(int i=0;i<count;i++)
+  {
+    if(arr[i] > maxx)
+    {
+      maxx = arr[i];
+    }
+  }
+  return maxx;
+}
